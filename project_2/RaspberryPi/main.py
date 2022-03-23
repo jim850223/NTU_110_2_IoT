f13ced4e8b1d9f4b4cb07965d2d736f72fc319fc
ddzzddzz <a1993311@gmail.com>
2022-03-23 17:44:25 +0800

    溫溼度、ini api
    add get/set  INI file API
    溫度濕度自動更新

--- project_2/RaspberryPi/main.py
+++ project_2/RaspberryPi/main.py
@@ -1,10 +1,13 @@
 #import RPi.GPIO as GPIO
 #import Adafruit_DHT
 from flask import Flask, render_template, request
-
+import configparser
 
 
 app = Flask(__name__)
+
+config_fileNmae = 'config.ini'
+config = configparser.ConfigParser()
 
 @app.route('/')
 def index():
@@ -12,9 +15,24 @@ def index():
 
 @app.route('/data/AM2302')
 def test():
-	return str(Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4))
+	# return str(Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 4))
+	return str([20, 42])
 
+@app.route('/config/<section>/<key>', methods=['GET'])
+def getConfig(section, key):
+	config.read(config_fileNmae)
+	return config.get(section, key, fallback='')
+@app.route('/config', methods=['POST'])
+def setConfig():
+	config.read(config_fileNmae)
 
+	for section in request.json.keys():
+		for key in request.json[section].keys():
+			config.set(section, key, request.json[section][key])
+	
+	with open(config_fileNmae, 'w') as configfile:
+		config.write(configfile)
+	return request.json
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80, debug=True)
--- project_2/RaspberryPi/static/js/index.js
+++ project_2/RaspberryPi/static/js/index.js
@@ -1,3 +1,7 @@
+const config = {
+    temperatureUnit: 1
+};
+
 $(function(){
     
     
@@ -7,5 +11,30 @@ $(function(){
         let dateStringWithTime = moment(now).format('HH:mm:ss');
         let timeElm = document.getElementById("time");
         timeElm.innerText = dateStringWithTime;
+        GetTempAndWet();
     }, 1000);
 });
+
+function GetTempAndWet() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET", "/data/AM2302", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let response = JSON.parse(this.responseText);
+            let temp = response[0];
+            let wet = response[1];
+
+            let temperatureElm = document.getElementById("temperature");
+            let wetElm = document.getElementById("wet");
+
+            if(config['temperatureUnit'] == '1')
+                temperatureElm.innerText = `室內溫度: ${temp}°C`;
+            else
+                temperatureElm.innerText = `室內溫度: ${temp * 9 / 5 + 32}°F`;
+            wetElm.innerText = `室內濕度: ${wet}%`;
+        }
+    };     
+}
