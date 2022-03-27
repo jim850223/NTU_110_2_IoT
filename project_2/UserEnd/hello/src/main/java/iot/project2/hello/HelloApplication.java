3029f563a331fd4ceebb36519c8222b45794e434
bHead_96 <chinghsiang2019@gmail.com>
2022-03-27 14:53:08 +0800

    Ready for pro_2

---
+++ project_2/UserEnd/hello/src/main/java/iot/project2/hello/HelloApplication.java
@@ -0,0 +1,26 @@
+package iot.project2.hello;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@SpringBootApplication
+@Controller
+public class HelloApplication {
+	@GetMapping("user")
+	@ResponseBody
+	public String user(String name) {
+		return String.format("hello!%s!", name);
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+
+}
---
+++ project_2/UserEnd/helloMaven/src/test/java/iot/project2/helloMaven/HelloMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package iot.project2.helloMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
