962188121ecd25ef3f7c2e9640fbc0cd56194ec9
ddzzddzz <a1993311@gmail.com>
2022-03-29 15:48:05 +0800

    add autorun

---
+++ project_2/RaspberryPi/autorun.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+chromium-browser  kiosk ignore-certificate-errors disable-restore-session-state http://127.0.0.1:80/
--- project_2/RaspberryPi/main.py
+++ project_2/RaspberryPi/main.py
@@ -86,4 +86,4 @@ def getInfo():
 
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80, debug=True)
+	app.run(host='127.0.0.1', port=80, debug=True)
